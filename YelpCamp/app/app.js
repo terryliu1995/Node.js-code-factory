b0d9b37f5d24dc243b8c658ff8fc22b42175ffa2
Terry Liu <zliu48@ncsu.edu>
2018-12-06 03:04:23 +0000

    finish yelpcamp app

--- YelpCamp/app/app.js
+++ YelpCamp/app/app.js
@@ -2,8 +2,10 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
     passport    = require("passport"),
     LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
     User        = require("./models/user"),
@@ -18,7 +20,9 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+app.use(methodOverride("_method"));
+app.use(flash());
+//seedDB();
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
@@ -35,8 +39,10 @@ passport.deserializeUser(User.deserializeUser());
 
 //pass current user to every template
 app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   next();
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
 });
 
 // use routers
--- YelpCamp/app/routes/index.js
+++ YelpCamp/app/routes/index.js
@@ -3,38 +3,37 @@ var router  = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 
+//root route
 router.get("/", function(req, res){
     res.render("landing");
 });
 
-
-// =====
-// Auth routes
-//show register form
+// show register form
 router.get("/register", function(req, res){
    res.render("register"); 
 });
 
-//handel sign up
-router.post("/register", function(req, res) {
+//handle sign up logic
+router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err){
-            console.log(err);
-            return res.render("register");
+            req.flash("error", err.message);
+            return res.render("/register");
         }
         passport.authenticate("local")(req, res, function(){
+           req.flash("success", "Welcome to YelpCamp " + user.username);
            res.redirect("/campgrounds"); 
         });
     });
 });
 
-//show login in form
+//show login form
 router.get("/login", function(req, res){
    res.render("login"); 
 });
 
-//login logic
+//handling login logic
 router.post("/login", passport.authenticate("local", 
     {
         successRedirect: "/campgrounds",
@@ -45,15 +44,10 @@ router.post("/login", passport.authenticate("local",
 // logout route
 router.get("/logout", function(req, res){
    req.logout();
+   req.flash("success", "Logged you out!");
    res.redirect("/campgrounds");
 });
 
-//middleware
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
+
 
 module.exports = router;
